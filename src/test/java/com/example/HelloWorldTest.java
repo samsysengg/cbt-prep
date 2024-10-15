0ddedee3116d44a60c8ba573f38403306a6555f5
samsysengg <33689874+samsysengg@users.noreply.github.com>
2024-10-15 20:12:54 +0530

    Create HelloWorldTest.java

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals("Hello, World!", helloWorld.sayHello());
+    }
+}
